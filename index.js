9c8f45d1c4fc8f7a3a06a240a0fae2142b762e16
jcstreet <jackiecstreet@gmail.com>
2019-12-19 13:01:39 -0500

    add directions API request

--- index.js
+++ index.js
@@ -8,58 +8,66 @@ const tripAdvisorKey = '1657e754d7msh055d850080cfb12p1fd65bjsnefdf7a34fe0c';
 const tripAdvisorHost = 'tripadvisor1.p.rapidapi.com';
 const tripAdvisorURL = 'https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng';
 
-let gUrl = '';
 let options = {};
-let restUrl = '';
+let restaurantUrl = '';
 
-function displayResults(responseJson) {
-    // clear old results out and previous error messages
-    //$('#results-list').empty();
-    //$('#js-error-message').empty();
-
-    //console.log(responseJson.data[0].fullName);
-
-    //for (let i = 0; i < responseJson.data.length; i++) {
-       // $('#results-list').append(
-            //`<li><h3>${responseJson.data[i].fullName}</h3>
-            //<p>${responseJson.data[i].description}</p>
-            //<a href=${responseJson.data[i].url} target="blank">${responseJson.data[i].url}</a>
-            //</li>`            
-    //)};
-    //$('#results').removeClass('hidden')
-}
-
-// function formatMapParams(params) {
-//     const queryItems = Object.keys(params)
-//         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-//     return queryItems.join('&');
-// }
-
-// function getMapData() {
-//     const mapURLTest = 'https://api.mapbox.com/directions/v5/mapbox/walking/-73.989%2C40.733%3B-74%2C40.733.json?access_token=pk.eyJ1IjoianN0cmVldHBob3RvIiwiYSI6ImNrNDBuaGc1ODAza2UzbG1zeTN4YmR6aTYifQ.jwuhK79huZWKto7UTfZvQQ';
-//     sendAPIRequest(mapURLTest);
-// }
-
-function formatRestData(responseJson) {
-    $('#results-list').empty();
-    $('#js-error-message').empty();
-
-    const restaurant = responseJson.data[0].name;
-    const restAddress = responseJson.data[0].address;
-    const restLat = responseJson.data[0].latitude;
-    const restLong = responseJson.data[0].longitude;
-
-    $('#results-list').append(
-        `<li><h3>${restaurant}</h3>
-            <p>${restAddress}</p>
-            <p>${restLat}</p>
-            <p>${restLong}</p>
-        </li>`            
+function displayTacoDirections(taco) {
+    $('#results').prepend(
+        `<p>A taco is only ${taco.miles} miles away.</p>
+        <p>It will take ${taco.minutes} minutes to walk there.</p>
+        `            
     );
     $('#results').removeClass('hidden');
 }
 
-function formatRestaruantParams(params) {
+function getMiles(meters) {
+    return meters * 0.00062137;
+}
+
+function getMinutes(seconds) {
+    return seconds / 60;
+}
+
+function handleMapResponse(responseJson) {
+    const tacoDirections = {
+        miles: getMiles(responseJson.routes[0].distance),
+        minutes: getMinutes(responseJson.routes[0].duration)
+    }
+
+    return tacoDirections;
+}
+
+function formatMapURL(long, lat, restaurant) {
+    const queryString = long + ',' + lat + ';' + restaurant.long + ',' + restaurant.lat;
+    const url = mapURL + encodeURIComponent(queryString) + '.json?access_token=' + mapToken;
+
+    return url;
+}
+
+function displayRestaurant(restaurant) {
+    $('#results').append(
+        `<h3>${restaurant.name}</h3>
+        <p>${restaurant.address}</p>
+        `            
+    );
+}
+
+function handleRestaurantResponse(responseJson) {
+    // clears old results and any previous error messages
+    $('#results').empty();
+    $('#js-error-message').empty();
+
+    const restaurant = {
+        name: responseJson.data[0].name,
+        address: responseJson.data[0].address,
+        lat: responseJson.data[0].latitude,
+        long: responseJson.data[0].longitude
+    }
+
+    return restaurant;
+}
+
+function formatRestaurantParams(params) {
     const queryItems = Object.keys(params)
         .map(key => `${key}=${params[key]}`)
     return queryItems.join('&');
@@ -73,8 +81,8 @@ function createRestaurantsParams(lat, long) {
         combined_food: '5110'   // Ensures we are only receiving results for mexican restaurants
     };
 
-    const queryString = formatRestaruantParams(params);
-    restUrl = tripAdvisorURL + '?' + queryString;
+    const queryString = formatRestaurantParams(params);
+    restaurantUrl = tripAdvisorURL + '?' + queryString;
     
     options = {
         method: 'GET',
@@ -82,28 +90,30 @@ function createRestaurantsParams(lat, long) {
             'x-rapidapi-host': tripAdvisorHost,
             'x-rapidapi-key': tripAdvisorKey})
     };
+
+    // create object and return it here
 }
 
-function createGeocodeURL(addr, geoURL, token) {
-    const reformatAddr = encodeURIComponent(addr);
-    gUrl = geoURL + reformatAddr + '.json?access_token=' + token; 
-}
-
-function getGeocode() {
+function getAllTheData(address) {
+    const gURL = createGeocodeURL(address, geocodingURL, mapToken);
     // Graps the lat and long from the geocoding API
-    sendAPIRequest(gUrl)
-    .then(responseJson => {
-        const long = responseJson.features[0].geometry.coordinates[0];
-        const lat = responseJson.features[0].geometry.coordinates[1];
+    sendAPIRequest(gURL)
+        .then(geoJson => {
+            const long = geoJson.features[0].geometry.coordinates[0];
+            const lat = geoJson.features[0].geometry.coordinates[1];
+            createRestaurantsParams(lat, long);
 
-        createRestaurantsParams(lat, long);
-        // grabs the closes restaurant from trip advisers API
-        sendAPIRequest(restUrl, options)
-        .then(responseJson => {
-            console.log(responseJson);
-            formatRestData(responseJson);
+            // grabs the closest restaurant from trip advisers API
+            sendAPIRequest(restaurantUrl, options)
+                .then(restaurantJson => {
+                    const restaurant = handleRestaurantResponse(restaurantJson);
+                    displayRestaurant(restaurant);
+                    return formatMapURL(long, lat, restaurant);
+                })
+                .then(sendAPIRequest)
+                .then(handleMapResponse)
+                .then(displayTacoDirections)
         })
-    })
         .catch(err => {
             $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
@@ -111,20 +121,25 @@ function getGeocode() {
 
 function sendAPIRequest(url, options) {
     return fetch(url, options)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-            throw new Error(response.statusText);
-        });
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+                throw new Error(response.statusText);
+            });
+}
+
+function createGeocodeURL(addr, geoURL, token) {
+    const reformatAddr = encodeURIComponent(addr);
+    const url = geoURL + reformatAddr + '.json?access_token=' + token; 
+    return url;
 }
 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
         const address = $('#adr').val() + ' ' + $('#city').val() + ' ' + $('#state').val() + ' ' + $('#zip').val();
-        createGeocodeURL(address, geocodingURL, mapToken);
-        getGeocode();
+        getAllTheData(address);
     });
 }
 
